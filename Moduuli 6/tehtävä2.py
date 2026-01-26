b99049e693da6199700ce246f1e8d7017397a783
Leo Hietasola <leo.hietasola@gmail.com>
2026-01-26 09:48:06 +0200

    korjattu

--- Moduuli 6/tehtävä2.py
+++ Moduuli 6/tehtävä2.py
@@ -1,11 +1,11 @@
 import random
 
-user_input = input("")
+user_input = int(input("Enter a number: "))
 def roll_dice(sides): #kun lisäät sides parametrin, osaa ohjelma heittää minkä tahansa sivusen nopan kanssa.
     return random.randint(1, sides)
 
 while True:
-    roll = roll_dice(10) #10 määrittää nopan sivun, jos olisi 6 olisi kuusisivuinen jne.
+    roll = roll_dice(user_input) #määrittää nopan sivun, jos olisi 6 olisi kuusisivuinen jne.
     print(roll)
-    if roll == 10:
+    if roll == user_input:
         break
