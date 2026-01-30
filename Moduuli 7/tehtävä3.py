17fa3d503361c3a6397114acf51a0c72dfe4d115
Leo Hietasola <leo.hietasola@gmail.com>
2026-01-30 23:25:31 +0200

    kesken

---
+++ Moduuli 7/tehtävä3.py
@@ -0,0 +1,9 @@
+
+
+print("Airprort Data Management")
+user_input = input("\n 1. Enter new aiport\n 2. Fetch airport information\n 3. Quit\n Please choose an option (1-3): ")
+
+if user_input == 1:
+
+
+
