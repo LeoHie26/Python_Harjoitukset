a8e02f522a1ce3e93fbd8a022ea254f080e04c0c
Leo Hietasola <leo.hietasola@gmail.com>
2026-01-31 01:09:25 +0200

    Valmis

--- Moduuli 7/tehtävä3.py
+++ Moduuli 7/tehtävä3.py
@@ -1,9 +1,33 @@
+airports = {}
 
+while True:
+    print("\nAirport Data Management")
+    user_input = input("1. Enter a new airport\n2. Fetch airport information\n3. Quit\nPlease choose an option (1-3): ")
 
-print("Airprort Data Management")
-user_input = input("\n 1. Enter new aiport\n 2. Fetch airport information\n 3. Quit\n Please choose an option (1-3): ")
+    if user_input == "1":
+        icao = input("Enter the ICAO code: ").strip().upper()
+        if icao == "":
+            print("ICAO code can not be empty.")
+            continue
 
-if user_input == 1:
+        airport_name = input("Enter the airport name: ").strip()
+        if airport_name == "":
+            print("Airport name can not be empty. ")
 
+        airports[icao] = airport_name
+        print(f"Airport {airport_name} with ICAO code {icao} has been added.")
 
+    elif user_input == "2":
+        icao = input("Enter the ICAO code: ")
+        if icao in airports:
+            print(f"The airport with ICAO code {icao} is {airports[icao]}.")
 
+        else:
+            print(f"No airport found with ICAO code {icao}.")
+
+    elif user_input == "3":
+        print("Thank you for using the Airport Data Management system. Goodbye!")
+        break
+
+    else:
+        print("Invalid option, please enter number between 1 to 3.")
