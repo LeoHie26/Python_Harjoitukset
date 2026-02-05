6d81efa6bf1e55eb9f692b04946968de570ae73e
Leo Hietasola <leo.hietasola@gmail.com>
2026-02-05 12:51:05 +0200

    Valmis. toimii pycharm muttei moodle

--- Moduuli 8/tehtävä3.py
+++ Moduuli 8/tehtävä3.py
@@ -11,21 +11,24 @@ connection = mysql.connector.connect(
             )
 
 def get_airport_coordinates(icao_code):
-    query = f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident = {icao_code1}"
+    query = f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident = '{icao_code}'"
     cursor = connection.cursor()
     cursor.execute(query)
     output = cursor.fetchone()
+    return output
 
 
-    if cursor.rowcount > 0:
-        print(distance.distance(coordinates1, coordinates2).km)
-        for distance in output:
-            print(f"Distance between {icao_code1} and {icao_code2}: {distance} kilometers")
+
 
 
 def run_airport_distance():
     icao_code1 = input("Enter the ICAO code of the first airport: ").upper()
     icao_code2 = input("Enter the ICAO code of the second airport: ").upper()
-    det_airport_coordinates(icao_code1, icao_code2)
+
+    coords1 = get_airport_coordinates(icao_code1)
+    coords2 = get_airport_coordinates(icao_code2)
+
+    distance_km = distance.distance(coords1, coords2).km
+    print(f"Distance between {icao_code1} and {icao_code2}: {distance_km:.2f} kilometers")
 
 run_airport_distance()
