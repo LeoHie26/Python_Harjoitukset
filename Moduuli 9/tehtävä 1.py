272db86e9e63b614150eb01a33b2d20fb1dec660
Leo Hietasola <leo.hietasola@gmail.com>
2026-03-18 11:07:23 +0200

    Toimivat ratkaisut tehtäviin 1-3.

---
+++ Moduuli 9/tehtävä 1.py
@@ -0,0 +1,19 @@
+class Car:
+    def __init__(self, license_plate, maximum_speed):
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+
+    current_speed = 0
+    travelled_distance = 0
+
+car = Car("ABC-123", "142 km/h")
+
+print(f"License plate: {car.license_plate}\n"
+      f"Maximum speed: {car.maximum_speed}\n"
+      f"Current speed: {car.current_speed} km/h\n"
+      f"Travelled distance: {car.travelled_distance} km")
+
+
+
+
+
---
+++ Moduuli 9/tehtävä 3.py
@@ -0,0 +1,20 @@
+class Car:
+    def __init__(self, license_plate, maximum_speed, current_speed=0, travelled_distance=0):
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+        self.current_speed = current_speed
+        self.travelled_distance = travelled_distance
+
+    def accelerate(self, acceleration):
+        if acceleration + self.current_speed >=0:
+            self.current_speed += acceleration
+
+        else:
+            self.current_speed = 0
+
+        if acceleration + self.current_speed >= self.maximum_speed:
+            self.current_speed = self.maximum_speed
+
+    def drive(self, time):
+        if time >= 0:
+            self.travelled_distance += self.current_speed * time
